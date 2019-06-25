cb0acf0af850c56dd693b19f91bd11203d2ba830
ntop <ntop.liu@gmail.com>
2019-06-25 14:29:16 +0800

    setup

---
+++ pages/index/index.js
@@ -0,0 +1,28 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    reply: {
+      index: -1,
+      subindex: -1,
+      hint: "",
+      text: "",
+      enable: true,
+    },
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    
+  },
+})
